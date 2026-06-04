5b2b1fe2ace336fefe6b59ede2ee000dbe9ad83c
Pawnee <pawneedefize@hotmail.com>
2026-06-04 14:29:05 +0200

    Fix pointer dereference in delete function

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	saved_head = *head;
 	p = 0;
-	while (p < index && tmp != NULL)
+	while (p < index && *head != NULL)
 	{
 		*head = (*head)->next;
 		p++;
